b9de8f2bbcdf23b8f420f2d7bc58d97b72678d3f
John Karahalis <john.karahalis@gmail.com>
2020-04-16 18:37:47 -0400

    Flesh out page.js example

--- src/routes/Routes.svelte
+++ src/routes/Routes.svelte
@@ -1,17 +1,23 @@
 <script>
 import page from 'page';
+
+// Pages
 import Index from './index.svelte';
 import Explore from './explore.svelte';
 
 let component;
-page('/', (p) => {
-  if (p.hash === 'explore') {
-    component = Explore;
-  } else {
-    component = Index;
-  }
-});
-page.start();
+let params;
+
+function setComponent(c) {
+  return function setComponentInner({ params: p }) {
+    component = c;
+    params = p;
+  };
+}
+
+page('/', setComponent(Index));
+page('/#explore', setComponent(Explore));
+page({ hashbang: true });
 </script>
 
-<svelte:component this={component} />
+<svelte:component this={component} bind:params />
