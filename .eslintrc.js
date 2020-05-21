86b754e2d943f2d0093c3b7055a31a513a5b6fcb
John Karahalis <john.karahalis@gmail.com>
2020-05-21 16:03:05 -0400

    Fix #35: Add an example test, remove unneeded deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,5 +57,13 @@ module.exports = {
         "no-multiple-empty-lines": ["error", { max: 2, maxBOF: 2, maxEOF: 0 }],
       },
     },
+    {
+      files: ["tests/**/*.test.js"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended", "plugin:jest/style"],
+      env: {
+        jest: true,
+      },
+    },
   ],
 };
