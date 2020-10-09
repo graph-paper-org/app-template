8403378140e2305bad190e2d7bc98e001138e3ab
William Lachance <wrlach@gmail.com>
2020-10-09 16:18:04 -0400

    Actually import and use graph paper in template
    * Import the graph paper stylesheet in main.js (a bit of a
      gotcha)
    * Add basic set of graph paper components
    * Include a line chart visualization in the main template

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -1,1 +1,14 @@
+<script>
+  import SomeComponent from "../components/SomeComponent.svelte";
+  import SomeGraph from "../components/SomeGraph.svelte";
+</script>
+
 <h2>Home</h2>
+
+<p>This is a component:</p>
+
+<SomeComponent />
+
+<p>And here's a graph-paper graph:</p>
+
+<SomeGraph />
