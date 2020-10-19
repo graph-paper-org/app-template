4d69b604e966def0c81ec255ac5ac67f19d2eac3
William Lachance <wrlach@gmail.com>
2020-10-19 11:03:00 -0400

    Actually import and use graph paper in template (#195)
    * Actually import and use graph paper in template
    
    * Import the graph paper stylesheet in main.js (a bit of a
      gotcha)
    * Add basic set of graph paper components
    * Include a line chart visualization in the main template
    
    * Fix tests
    
    * Simplify line example

---
+++ src/components/SomeGraph.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { DataGraphic } from "@graph-paper/datagraphic";
+  import { Line } from "@graph-paper/elements";
+  import { LeftAxis, BottomAxis } from "@graph-paper/guides";
+
+  export let points = 100;
+
+  let y = 100;
+
+  const data = Array.from({ length: points }).map((_, x) => {
+    y += (Math.random() - 0.5) * 80;
+    return { x, y };
+  });
+</script>
+
+<DataGraphic>
+  <LeftAxis />
+  <BottomAxis />
+  <Line {data} />
+</DataGraphic>
